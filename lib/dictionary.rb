723c2a7083ff1970bf6ce2068deb9a0e4c2ee2b2
Alex G. Lacasa <aloon.com@gmail.com>
2012-10-27 01:41:58 +0200

    initial import

---
+++ lib/dictionary.rb
@@ -0,0 +1,32 @@
+class Dictionary
+	attr_reader :id, :desc
+
+	def initialize id, desc
+         @id = id
+         @desc = desc
+    end
+
+    def self.add_item(key,value)
+        @hash ||= {}
+        @hash[key]=value
+    end
+
+    def self.const_missing(key)
+        @hash[key]
+    end    
+
+    def self.each
+        @hash.each {|key,value| yield(key,value)}
+    end
+
+    def self.get key=nil
+    	if key.nil?
+    		status=[]
+	        each { |k,v| status << new(v,k[0..-1]) }
+	        status
+    	else
+    		each { |k,v| return new(v,k[0..-1]) if v==key  }
+    	end
+    	
+    end
+end
---
+++ lib/has_dictionary.rb
@@ -0,0 +1,26 @@
+module ActiveRecord
+
+  module Dictionaries
+    
+    def self.included( base ) #:nodoc:
+      base.send( :extend, ActsMethods )
+    end
+    
+    module ActsMethods #:nodoc:
+      
+      def has_dictionary(options = {})
+        class_attribute :dic_options
+
+        self.dic_options = options
+
+        define_method(dic_options[:method]) {
+          eval "#{dic_options[:dic_class]}.get self.#{dic_options[:field]}"
+        }
+      end
+
+    end
+    
+  end
+end
+
+ActiveRecord::Base.send( :include, ActiveRecord::Dictionaries )
