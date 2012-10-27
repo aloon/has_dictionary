1a670aee0bd8ea500c6aff6565a81ed63a6c355e
Alex G. Lacasa <aloon.com@gmail.com>
2012-10-27 02:15:23 +0200

    creating correcty the gem

--- lib/has_dictionary.rb
+++ lib/has_dictionary.rb
@@ -1,26 +1,4 @@
-module ActiveRecord
-
-  module Dictionaries
-    
-    def self.included( base ) #:nodoc:
-      base.send( :extend, ActsMethods )
-    end
-    
-    module ActsMethods #:nodoc:
-      
-      def has_dictionary(options = {})
-        class_attribute :dic_options
-
-        self.dic_options = options
-
-        define_method(dic_options[:method]) {
-          eval "#{dic_options[:dic_class]}.get self.#{dic_options[:field]}"
-        }
-      end
-
-    end
-    
-  end
-end
+require 'has_dictionary/has_dictionary'
+require 'has_dictionary/has_dictionary'
 
 ActiveRecord::Base.send( :include, ActiveRecord::Dictionaries )
